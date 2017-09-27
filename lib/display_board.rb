f4c008594c91fdac54bd8d86a4abb84329bcd2e0
Man Leung <lepusleung@gmail.com>
2017-09-27 19:04:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -32,6 +32,7 @@ display_board(board)
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 board[0] = "O"
 board[4] = "X"
+def display_board(board)
   puts  " O |   |   "
   puts  "-----------"
   puts  "   | X |   "
